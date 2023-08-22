80e05fb2d9beca3c2a2349b2a1e74e6337f76a8e
tokdaniel <tokdaniel4@gmail.com>
2023-08-22 14:14:22 +0200

    remove vercel edge

--- src/entry.ssr.tsx
+++
@@ -1,6 +0,0 @@
-import { renderToString, type RenderOptions } from "@builder.io/qwik/server";
-import { App } from "./app";
-
-export default function (opts: RenderOptions) {
-  return renderToString(<App />, opts);
-}
